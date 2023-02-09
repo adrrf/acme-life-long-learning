c15af7c739cb64a2630f16c6b25b416a15cde5da
adrrf <adrromflo@alum.us.es>
2023-02-09 16:33:04 +0100

    feat(project): initial commit

---
+++ utils/create-database.sql
@@ -0,0 +1,19 @@
+drop database if exists `Acme-L3-D1`;
+create database `Acme-L3-D1`
+        character set = 'utf8mb4'
+        collate = 'utf8mb4_unicode_ci';
+
+grant select, insert, update, delete, create, drop, references, index, alter, 
+        create temporary tables, lock tables, create view, create routine, 
+        alter routine, execute, trigger, show view
+    on `Acme-L3-D1`.* to 'acme-user'@'%';
+
+drop database if exists `Acme-L3-D1-Test`;
+create database `Acme-L3-D1-Test`
+	character set = 'utf8mb4'
+  	collate = 'utf8mb4_unicode_ci';
+
+grant select, insert, update, delete, create, drop, references, index, alter, 
+        create temporary tables, lock tables, create view, create routine, 
+        alter routine, execute, trigger, show view
+    on `Acme-L3-D1-Test`.* to 'acme-user'@'%';
