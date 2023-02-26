e7ebc1b8ff708f0c306cd0da419c8516115ba74d
adrrf <adrromflo@alum.us.es>
2023-02-26 11:22:53 +0100

    feat(roles): added assistant

---
+++ src/main/java/acme/roles/Assistant.java
@@ -0,0 +1,35 @@
+
+package acme.roles;
+
+import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.URL;
+
+import acme.framework.data.AbstractRole;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Assistant extends AbstractRole {
+
+	private static final long	serialVersionUID	= 1L;
+
+	@NotBlank
+	@Length(max = 76)
+	protected String			supervisor;
+
+	@NotBlank
+	@Length(max = 101)
+	protected String			fields;
+
+	@NotBlank
+	@Length(max = 101)
+	protected String			resume;
+
+	@URL
+	protected String			link;
+}
