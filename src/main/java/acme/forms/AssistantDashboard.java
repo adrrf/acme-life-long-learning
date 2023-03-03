19c276e42b42561219f480b8fb5680dd35c222ac
adrrf <adrromflo@alum.us.es>
2023-03-03 14:59:10 +0100

    feat(d2-s3): assistant dashboard

---
+++ src/main/java/acme/forms/AssistantDashboard.java
@@ -0,0 +1,36 @@
+
+package acme.forms;
+
+import java.util.Map;
+
+import javax.persistence.OneToOne;
+import javax.validation.Valid;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+
+import acme.framework.data.AbstractForm;
+import acme.roles.Assistant;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class AssistantDashboard extends AbstractForm {
+
+	private static final long	serialVersionUID	= 1L;
+
+	@Min(0)
+	@NotNull
+	Integer						nTutorials;
+
+	@NotNull
+	Map<String, Integer>		statsSessionTime;
+
+	@NotNull
+	Map<String, Integer>		statsTutorialTime;
+
+	@Valid
+	@NotNull
+	@OneToOne(optional = false)
+	Assistant					assistant;
+}
