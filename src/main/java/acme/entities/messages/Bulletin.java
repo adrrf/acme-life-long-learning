36a7fee3428a714e1a4377742bf662665aef42cc
AMC112002 <alejandromateocapilla@gmail.com>
2023-03-15 16:33:18 +0100

    feat(d2-s5): Group task (Bulletin)

---
+++ src/main/java/acme/entities/messages/Bulletin.java
@@ -0,0 +1,43 @@
+
+package acme.entities.messages;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Past;
+
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.URL;
+
+import acme.framework.data.AbstractEntity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Bulletin extends AbstractEntity {
+
+	private static final long	serialVersionUID	= 1L;
+
+	@NotNull
+	@Past
+	protected Date				instantionMoment;
+
+	@NotBlank
+	@Length(max = 76)
+	protected String			title;
+
+	@NotBlank
+	@Length(max = 101)
+	protected String			message;
+
+	@NotNull
+	protected Boolean			flag;
+
+	@URL
+	protected String			link;
+
+}
