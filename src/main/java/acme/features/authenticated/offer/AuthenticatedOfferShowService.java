60281b5f2ae44cb3b07caad0bf9239f658c66cf0
angmunpri <angmunpri@alum.us.es>
2023-05-25 15:53:01 +0200

    Changes in offer

---
+++ src/main/java/acme/features/authenticated/offer/AuthenticatedOfferShowService.java
@@ -0,0 +1,63 @@
+
+package acme.features.authenticated.offer;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import acme.entities.messages.Offer;
+import acme.framework.components.accounts.Authenticated;
+import acme.framework.components.models.Tuple;
+import acme.framework.services.AbstractService;
+
+@Service
+public class AuthenticatedOfferShowService extends AbstractService<Authenticated, Offer> {
+
+	@Autowired
+	protected AuthenticatedOfferRepository repository;
+
+
+	@Override
+	public void check() {
+		boolean status;
+
+		status = super.getRequest().hasData("id", int.class);
+
+		super.getResponse().setChecked(status);
+	}
+
+	@Override
+	public void authorise() {
+		boolean status;
+		int offerId;
+		Offer offer;
+
+		offerId = super.getRequest().getData("id", int.class);
+		offer = this.repository.findOfferById(offerId);
+		status = offer != null;
+
+		super.getResponse().setAuthorised(status);
+	}
+
+	@Override
+	public void load() {
+		Offer object;
+		int id;
+
+		id = super.getRequest().getData("id", int.class);
+		object = this.repository.findOfferById(id);
+
+		super.getBuffer().setData(object);
+	}
+
+	@Override
+	public void unbind(final Offer object) {
+		assert object != null;
+
+		Tuple tuple;
+
+		tuple = super.unbind(object, "instantiationMoment", "heading", "summary", "startDate", "endDate", "price", "link");
+
+		super.getResponse().setData(tuple);
+	}
+
+}
